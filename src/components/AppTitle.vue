c2e0510be35943a16ea84f2bb2728ffae1c80290
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-14 17:10:55 +0200

    update

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Hello, Vue!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  
+ 
+    data() {
+      return {
+        message: 'Welcome to my Vue app!'
+      };
+    }
+  };
+  </script>
+  
+  <style>
+
+  </style>
