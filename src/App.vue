cd2fc868d6b9f20cb513dd3ea6a2919b459028c0
manuelcamaioni <camaioni.manuel@gmail.com>
2023-06-14 18:01:10 +0200

    first basic implementation of title and jumbotron in SFC

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-
+    <AppTitle />
+    <Hero />
 </template>
 
 <script>
-import AppTitle from './components/AppTitle.vue';
+import AppTitle from "./components/AppTitle.vue";
+import Hero from "./components/HeroApp.vue";
 export default {
+    components: {
+        AppTitle,
+        Hero,
+    },
 
-  components:{
-    AppTitle
-  },
-
-  data() {
-    return {
-
-    };
-  }
+    data() {
+        return {};
+    },
 };
 </script>
 
 <style>
-
+div {
+    margin: 0;
+    width: 100%;
+}
 </style>
---
+++ src/components/HeroApp.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="jumbo"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
+<style>
+.jumbo {
+    width: 70%;
+    height: 500px;
+    margin: auto;
+    background-color: rgb(99, 63, 197);
+    border-radius: 0.25rem;
+}
+</style>
